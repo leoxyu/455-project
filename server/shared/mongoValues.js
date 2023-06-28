aa7e83684e3bb229cc4d2f18a7f4bbd2abcc07a6
Leo Yu <lleeooyu@gmail.com>
2023-06-27 20:03:06 -0700

    mongodb changes

---
+++ server/shared/mongoValues.js
@@ -0,0 +1,4 @@
+export const URI = 'mongodb+srv://unifi:smOSKX8hMAYZhUFD@cluster.cojsppq.mongodb.net/?retryWrites=true&w=majority';
+export const DATABASE_NAME = 'unifi';
+export const USER_COLLECTION = 'users';
+export const PLAYLIST_COLLECTION = 'playlists';
