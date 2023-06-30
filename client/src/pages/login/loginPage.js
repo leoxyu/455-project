0c062f5d68e0114a79f8196bf7af7ff832b4d294
Ning114 <david.leng114@gmail.com>
2023-06-29 17:35:50 -0700

    Completed OAuth for youtube

--- client/src/pages/login/loginPage.js
+++ client/src/pages/login/loginPage.js
@@ -50,26 +50,68 @@ const LoginPage = () => {
 
   const dispatch = useDispatch();
 
-  // for grabbing data from URL
-  const urlParams = new URLSearchParams(window.location.search);
-
   // ===============================================================================================
-  // OAuth stuff 
+  // OAuth youtube
   // ===============================================================================================
 
   useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.hash);
+
+    const youtube_token_session = sessionStorage.getItem(SESSION_STORAGE_KEYS.youtubeAccessToken);
+    // const youtube_rtoken_session = sessionStorage.getItem(SESSION_STORAGE_KEYS.youtubeRefreshToken);
+
+    if (youtube_token_session) {
+      dispatch(setAccessTokenYoutube(youtube_token_session));
+      // dispatch(setRefreshTokenYoutube(youtube_rtoken_session));
+      setYoutubeLoggedIn(true);
+    }
+
+    const access_token = urlParams.get('access_token');
+    // const refresh_token = urlParams.get('refresh_token'); google Oauth doesn't send you a refresh token idk
+    const token_type = urlParams.get('token_type');
+    const error = urlParams.get('error');
+
+    console.log('youtube OAuth token type: ', token_type);
+
+    if (error) {
+      console.log("youtube OAuth error: ", error);
+      return;
+    } else if (access_token && token_type === 'Bearer') {
+      dispatch(setAccessTokenYoutube(access_token));
+      // dispatch(setAccessTokenYoutube(refresh_token));
+      // dispatch(setYoutubeAuthError(error));
+
+      sessionStorage.setItem(SESSION_STORAGE_KEYS.youtubeAccessToken, access_token);
+      // sessionStorage.setItem(SESSION_STORAGE_KEYS.youtubeRefreshToken, refresh_token);
+
+      setYoutubeLoggedIn(true);
+      console.log('youtube successfully logged in');
+
+    } else {
+      console.log("Something went wrong with youtube OAuth and retrieving tokens from URl");
+    }
+
+  }, []);
+
+  // ===============================================================================================
+  // OAuth spotify
+  // ===============================================================================================
+
+  useEffect(() => {
+    // for grabbing data from URL
+    const urlParams = new URLSearchParams(window.location.search);
+
     // always check session storage first for keys from previous OAuth. It's fine if one of them gets overwritten in sessionStorage
     // by the new token in URL
     const spotify_token_session = sessionStorage.getItem(SESSION_STORAGE_KEYS.spotifyAccessToken);
     const spotify_rtoken_session = sessionStorage.getItem(SESSION_STORAGE_KEYS.spotifyRefreshToken);
 
-    const youtube_token_session = sessionStorage.getItem(SESSION_STORAGE_KEYS.youtubeAccessToken);
-    const youtube_rtoken_session = sessionStorage.getItem(SESSION_STORAGE_KEYS.youtubeRefreshToken);
+    if (spotify_token_session && spotify_rtoken_session) {
+      dispatch(setAccessTokenSpotify(spotify_token_session));
+      dispatch(setRefreshTokenSpotify(spotify_rtoken_session));
+      setSpotifyLoggedIn(true);
+    }
 
-    if (spotify_token_session) dispatch(setAccessTokenSpotify(spotify_token_session));
-    if (spotify_rtoken_session) dispatch(setRefreshTokenSpotify(spotify_rtoken_session));
-    if (youtube_token_session) dispatch(setAccessTokenYoutube(youtube_token_session));
-    if (youtube_rtoken_session) dispatch(setRefreshTokenYoutube(youtube_rtoken_session));
 
     // check URL for OAuth tokens
 
@@ -94,16 +136,7 @@ const LoginPage = () => {
       sessionStorage.setItem(SESSION_STORAGE_KEYS.spotifyRefreshToken, refresh_token);
 
       setSpotifyLoggedIn(true);
-
-    } else if (type === "youtube") {
-      dispatch(setAccessTokenYoutube(access_token));
-      dispatch(setAccessTokenYoutube(refresh_token));
-      dispatch(setYoutubeAuthError(error));
-
-      sessionStorage.setItem(SESSION_STORAGE_KEYS.youtubeAccessToken, access_token);
-      sessionStorage.setItem(SESSION_STORAGE_KEYS.youtubeRefreshToken, refresh_token);
-
-      setYoutubeLoggedIn(true);
+      console.log('spotify successfully logged in');
 
     } else {
       console.log("Something went wrong with spotify OAuth and retrieving tokens from URl");
@@ -218,6 +251,11 @@ const LoginPage = () => {
       <br></br>
       {!spotifyLoggedIn ? (
         <button onClick={spotifyLogin} className="login-button">Verify Spotify Account</button>
+      ) : (
+        <p>Youtube connected</p>
+      )}
+      {!youtubeLoggedIn ? (
+        <button onClick={youtubeLogin} className="login-button">Verify Youtube Account</button>
       ) : (
         <p>Spotify connected</p>
       )}
