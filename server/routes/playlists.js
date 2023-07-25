be3b184cac5397f601453bed7ca7030bd76fce66
Ning114 <david.leng114@gmail.com>
2023-07-25 16:22:47 -0700

    Added support for importing albums to importManySpotify (Untested)

--- server/routes/playlists.js
+++ server/routes/playlists.js
@@ -260,6 +260,10 @@ playlistsRouter.delete('/:playlistID/songs/:songID', async (req, res, next) => {
 // fetches all tracks for a single playlist
 
 function getTracksHelper(access_token, next, playlist) {
+
+  console.log("\r\ninside getTrackHelper");
+  console.log("\r\nplaylist.isAlbum: ", playlist.isAlbum);
+
   // if next link is null, dont do anything
   return next ? fetch(next, {
     method: "GET",
@@ -273,19 +277,39 @@ function getTracksHelper(access_token, next, playlist) {
   }).then(data => {
     // console.log(data);
     for (const i of data.items) {
-      playlist.songs.push(
-        {
+
+      if (playlist.isAlbum) {
+        // playlist is TYPE_ALBUM
+        const parsedTrack = {
+          songID: uuid(),
+          artist: i.artists[0].name,
+          name: i.name,
+          type: TYPE_SPOTIFY,
+          link: i.uri,
+          imageLink: playlist.coverImageURL,
+          album: playlist.name,
+          duration: i.duration_ms,
+          releaseDate: playlist.dateCreated,
+        };
+        console.log(parsedTrack);
+        playlist.songs.push(parsedTrack);
+      } else {
+        // playlist is TYPE_PLAYLIST
+        const parsedTrack = {
           songID: uuid(),
           artist: i.track.artists[0].name,
           name: i.track.name,
-          type: 'spotify',
+          type: TYPE_SPOTIFY,
           link: i.track.uri,
           imageLink: i.track.album.images[0].url,
           album: i.track.album.name,
           duration: i.track.duration_ms,
           releaseDate: i.track.album.release_date,
-        }
-      );
+        };
+        console.log(parsedTrack);
+        playlist.songs.push(parsedTrack);
+      }
+
     }
     if (data.next) {
       return getTracksHelper(access_token, data.next, playlist);
@@ -302,13 +326,26 @@ playlistsRouter.post('/importManySpotify', async (req, res, next) => {
 
   console.log("inside importManySpotify");
 
-  const { playlists, access_token } = req.body;
 
-  let queryUrl;
+  const { playlists, accessToken } = req.body;
+
+  // console.log("\r\nreq body: ");
+  // console.log(req.body);
+
+  // console.log("\r\nplaylistIDs: ");
+  // console.log(playlists);
+
+  // console.log("\r\naccess_token: ");
+  // console.log(accessToken);
+
+
 
   Promise.allSettled(playlists?.map(playlist => {
 
-    const type = playlist.type;
+    const type = playlist.playlistType;
+    console.log(type);
+
+    let queryUrl;
 
     if (type === TYPE_PLAYLIST) queryUrl = `https://api.spotify.com/v1/playlists/${playlist.id}`;
     else if (type === TYPE_ALBUM) queryUrl = `https://api.spotify.com/v1/albums/${playlist.id}`;
@@ -316,18 +353,19 @@ playlistsRouter.post('/importManySpotify', async (req, res, next) => {
       return res.status(400).send({ error: "one of the playlists ID's had invalid type (not playlist or album)" });
     }
 
+
     return fetch(queryUrl, {
       method: "GET",
-      headers: { Authorization: `Bearer ${access_token}` }
+      headers: { Authorization: `Bearer ${accessToken}` }
     }).then(response => {
       if (response.status === 200) {
         return response.json();
       } else {
         return Promise.reject(response);
       }
-    }).then(data => getTracksHelper(access_token, data.tracks.href, {
+    }).then(data => getTracksHelper(accessToken, data.tracks.href, {
       playlistID: uuid(),
-      dateCreated: new Date(),
+      dateCreated: data.type === TYPE_ALBUM ? data.release_date : new Date(), // sets to releaseDate if album. Playlist don't have a release date, so just set to import/creation time
       description: data.description,
       name: data.name,
       author: new ObjectId(), // TODO: objectid of the user who is importing the playlist
@@ -335,17 +373,18 @@ playlistsRouter.post('/importManySpotify', async (req, res, next) => {
       coverImageURL: data.images[0].url,
       songs: [],
       originSpotifyId: data.id,
-      isAlbum: false,
+      isAlbum: data.type === TYPE_ALBUM ? true : false,
     })
     ).then(async (playlist) => {
+      // console.log(playlist);
       const result = await playlistsCol.insertOne(playlist);
       console.log(`inserted ${result.insertedId}`);
       return playlist.originSpotifyId; // useful for frontend retry
     })
   }))
     .then(outcomes => {
-      console.log(outcomes);
       if (!outcomes.some((o) => o.status === "fulfilled")) {
+        console.log(outcomes);
         return res.status(500).send(outcomes);
       }
       return res.status(200).send(outcomes);
