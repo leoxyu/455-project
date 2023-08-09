5c053c728072b9bce8253d354d0059e22bd81e62
Moses Hsu <67880280+moseshsu01@users.noreply.github.com>
2023-08-08 22:06:22 -0700

    fix page selector positioning bug

--- client/src/pages/library/libraryPage.js
+++ client/src/pages/library/libraryPage.js
@@ -14,12 +14,15 @@ const LibraryPage = () => {
 
   return (
     <div className='library-page'>
-      <div className='page-selector'>
-        <div className='library-button' onClick={() => handlePageChange('Playlists')} data-selected={selectedPage === 'Playlists' ? 'true' : 'false'}>
-          <PlaylistIcon className='playlist-library-icon' />
-        </div>
-        <div className='library-button' onClick={() => handlePageChange('Songs')} data-selected={selectedPage === 'Songs' ? 'true' : 'false'}>
-          <SongIcon className='song-library-icon' />
+      <div className='page-selector-container'>
+        <div className='page-selector-separator'/>
+        <div className='page-selector'>
+          <div className='library-button' onClick={() => handlePageChange('Playlists')} data-selected={selectedPage === 'Playlists' ? 'true' : 'false'}>
+            <PlaylistIcon className='playlist-library-icon' />
+          </div>
+          <div className='library-button' onClick={() => handlePageChange('Songs')} data-selected={selectedPage === 'Songs' ? 'true' : 'false'}>
+            <SongIcon className='song-library-icon' />
+          </div>
         </div>
       </div>
       {selectedPage === 'Playlists' ? <PlaylistsPage /> : <SongPage />}
